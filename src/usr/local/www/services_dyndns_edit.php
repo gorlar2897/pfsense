15de2f7bff96c953ae9d407fe21df89d1f353a6d
gorlar2897 <gorlar2897@gmail.com>
2026-06-23 15:06:37 +0200

    Add loopiaapi support to services_dyndns_edit.php

--- src/usr/local/www/services_dyndns_edit.php
+++ src/usr/local/www/services_dyndns_edit.php
@@ -103,6 +103,7 @@ if ($_POST['save'] || $_POST['force']) {
 		"googledomains" => array("apex" => false, "wildcard" => true, "username_none" => false),
 		"linode" => array("apex" => false, "wildcard" => false, "username_none" => true),
 		"linode-v6" => array("apex" => false, "wildcard" => false, "username_none" => true),
+		"loopiaapi" => array("apex" => true, "wildcard" => false, "username_none" => false),
 		"luadns" => array("apex" => true, "wildcard" => true, "username_none" => false),
 		"luadns-v6" => array("apex" => true, "wildcard" => true, "username_none" => false),
 		"namecheap" => array("apex" => true, "wildcard" => true, "username_none" => true),

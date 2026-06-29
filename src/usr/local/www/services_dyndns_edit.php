e7cb58487f4c82058d640168d4a3c3b7c4ceabdb
gorlar2897 <gorlar2897@gmail.com>
2026-06-29 21:58:18 +0200

    Update Loopia API settings in services_dyndns_edit.php

--- src/usr/local/www/services_dyndns_edit.php
+++ src/usr/local/www/services_dyndns_edit.php
@@ -103,7 +103,7 @@ if ($_POST['save'] || $_POST['force']) {
 		"googledomains" => array("apex" => false, "wildcard" => true, "username_none" => false),
 		"linode" => array("apex" => false, "wildcard" => false, "username_none" => true),
 		"linode-v6" => array("apex" => false, "wildcard" => false, "username_none" => true),
-		"loopia-api" => array("apex" => true, "wildcard" => false, "username_none" => false),
+		"loopia-api" => array("apex" => true, "wildcard" => true, "username_none" => false),
 		"luadns" => array("apex" => true, "wildcard" => true, "username_none" => false),
 		"luadns-v6" => array("apex" => true, "wildcard" => true, "username_none" => false),
 		"namecheap" => array("apex" => true, "wildcard" => true, "username_none" => true),
@@ -406,7 +406,7 @@ $group->add(new Form_Input(
 ));
 
 $group->setHelp('Enter the complete fully qualified domain name. Example: myhost.dyndns.org%1$s' .
-				'Azure, Cloudflare, Linode, LuaDNS, Porkbun: Name.com: Enter @ as the hostname to indicate an empty field.%1$s' .
+				'Azure, Cloudflare, Linode, Loopia-API LuaDNS, Porkbun: Name.com: Enter @ as the hostname to indicate an empty field.%1$s' .
 				'deSEC: Enter the FQDN.%1$s' .
 				'DNSimple: Enter only the domain name.%1$s' .
 				'DNS Made Easy: Dynamic DNS ID (NOT hostname)%1$s' .

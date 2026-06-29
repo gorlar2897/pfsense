fbfee438cf22c3592382d3e301b01fdddb266eab
gorlar2897 <gorlar2897@gmail.com>
2026-06-29 21:21:31 +0200

    Add error indication classes for dyndns service
    Added icon and text classes for error indication.

--- src/usr/local/www/services_dyndns.php
+++ src/usr/local/www/services_dyndns.php
@@ -113,6 +113,9 @@ foreach (config_get_path("dyndnses/dyndns", []) as $dyndns):
 	}
 	$filename = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($hostname) . "{$dyndns['id']}.cache";
 	$filename_v6 = "{$g['conf_path']}/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($hostname) . "{$dyndns['id']}_v6.cache";
+	$icon_class = "fa-solid fa-times-circle";
+	$text_class = "text-danger";
+	$icon_title = "Failed";
 	if (file_exists($filename)) {
 		$ipaddr = dyndnsCheckIP($dyndns['interface'], array_get_path($dyndns, 'check_ip_mode'), AF_INET);
 		$cached_ip_s = explode("|", file_get_contents($filename));
